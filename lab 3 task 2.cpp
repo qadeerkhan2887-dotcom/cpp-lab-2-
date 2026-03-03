bfe0bd3c9668f8f93fbb8c979895184389784aaf
qadeerkhan2887-dotcom <qadeerkhan2887@gmail.com>
2026-03-04 00:50:38 +0500

    Add files via upload

---
+++ lab 3 task 2.cpp
@@ -0,0 +1,24 @@
+muhammad qadeer khan 
+etea id 06067
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    // Prompt user to enter an integer
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    // Use logical operators to check conditions
+    if (num > 0 && num < 100) {
+        cout << "The number is greater than 0 and less than 100." << endl;
+    } else {
+        cout << "The number does not meet both conditions (greater than 0 and less than 100)." << endl;
+    }
+
+    return 0;
+}
+/*this program display the number in between zero to hundred and cannot display the number greater than hundred and less than zero
+
+
